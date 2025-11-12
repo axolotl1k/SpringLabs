152e442ff96c236e88bd8ca99fb98a417110831a
axolotlik <slsanya2006@gmail.com>
2025-11-12 19:03:45 +0200

    initial commit

---
+++ src/main/java/org/axolotlik/labs/GradebookApplication.java
@@ -0,0 +1,13 @@
+package org.axolotlik.labs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JournalApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JournalApplication.class, args);
+    }
+
+}
